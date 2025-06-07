2e90491f9c46e9572681d97edae6fa9b755204eb
Chris <chriswangwch@gmail.com>
2025-06-07 21:44:42 +0800

    refactor file structure

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  srcDir: "src",
   modules: ["@wxt-dev/module-react"],
   hooks: {
     "build:done": async (_wxt, _output) => {
