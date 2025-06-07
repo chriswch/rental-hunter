4610993eda7072cd94d6162d8245f7a55ad43c9a
Chris <chriswangwch@gmail.com>
2025-06-07 21:18:47 +0800

    copy .output/ to windows
    for WSL.
    from ubuntu to windows.
    because chrome 137 can't read files in WSL

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,18 @@
-import { defineConfig } from 'wxt';
+import { cp } from "node:fs/promises";
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-react'],
+  modules: ["@wxt-dev/module-react"],
+  hooks: {
+    "build:done": async (_wxt, _output) => {
+      await cp(
+        ".output/chrome-mv3-dev/",
+        "/mnt/c/Users/wmike/Downloads/chrome-mv3-dev",
+        {
+          recursive: true,
+        }
+      );
+    },
+  },
 });
