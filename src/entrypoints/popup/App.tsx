08dceaf967a344520f64a2e6b6f121c9ac07b4d0
Chris <chriswangwch@gmail.com>
2025-06-09 02:40:57 +0800

    Tailwind setup

--- src/entrypoints/popup/App.tsx
+++ src/entrypoints/popup/App.tsx
@@ -1,10 +1,4 @@
-import { useState } from "react";
-
-import reactLogo from "@/assets/react.svg";
 import { MessageType, StartScrapeMessage } from "@/types/messages";
-
-import "./App.css";
-import wxtLogo from "/wxt.svg";
 
 const queryCurrentTabId = async (): Promise<number | undefined> => {
   const [tab] = await browser.tabs.query({ active: true, currentWindow: true });
@@ -25,31 +19,10 @@ const handleScrapeStart = async () => {
 };
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://wxt.dev" target="_blank" rel="noreferrer">
-          <img src={wxtLogo} className="logo" alt="WXT logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>WXT + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <button onClick={handleScrapeStart}>Start scraping</button>
-      <p className="read-the-docs">
-        Click on the WXT and React logos to learn more
-      </p>
     </>
   );
 }
