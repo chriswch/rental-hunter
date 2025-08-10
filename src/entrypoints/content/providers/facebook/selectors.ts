46400c8d4b8c2f5adbd82155595a07c10d77edb4
chriswch <chriswangwch@gmail.com>
2025-08-10 17:48:45 +0800

    constants for fb dom selectors

---
+++ src/entrypoints/content/providers/facebook/selectors.ts
@@ -0,0 +1,18 @@
+// Facebook DOM selectors
+
+export const FEED_CONTAINER_SELECTOR = 'div[role="feed"]';
+export const POST_ROOT_SELECTOR = "div.x1yztbdb.x1n2onr6.xh8yej3.x1ja2u2z";
+
+export const STORY_MESSAGE_SELECTOR =
+  'div[data-ad-rendering-role="story_message"]';
+
+export const TRANSLATED_BLOCKQUOTE_SELECTOR =
+  'blockquote[class="html-blockquote xexx8yu x18d9i69 x12u81az x1t7ytsu x56jcm7 x14z9mp xieb3on x1gslohp xv54qhq xf7dkkf xyqm7xq"]';
+
+export const PROFILE_NAME_SELECTOR =
+  'div[data-ad-rendering-role="profile_name"]';
+
+export const PRICE_CONTAINER_SELECTOR =
+  'span[class="html-span xdj266r x14z9mp xat24cr x1lziwak xexx8yu xyri2b x18d9i69 x1c1uobl x1hl2dhg x16tdsg8 x1vvkbs xtvhhri"]';
+
+export const IMAGE_ANCHOR_SELECTOR = "a[aria-label][attributionsrc]";
